e43828012268657306357823d63bf92070cbb0c2
Piszmog <piszmogcode@gmail.com>
2020-01-27 16:56:14 -0700

    Remove quotes if in dog name

--- run.go
+++ run.go
@@ -69,7 +69,7 @@ func RunDogDownloads(dogs string, baseDirectory string) error {
 			return
 		}
 		name := e.ChildText(header3)
-		dogName := strings.ToLower(name)
+		dogName := strings.ReplaceAll(strings.ToLower(name), "\"", "")
 		dogMatch := dogMap.IsMatch(dogName)
 		// If a match then create dir and description.txt file
 		if dogMatch {
